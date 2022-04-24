452a4bb724300539b3ae585f7aca77512e306e6b
Jan Kowalleck <jan.kowalleck@gmail.com>
2022-04-24 23:07:37 +0200

    `@cyclonedx/bom:^3.8` & node `>= 12.0.0` & tests (#51)
    fixes #50
    
    * Changed
      * Requires node `>= 12.0.0` now, was `8.0.0`. (via [#287])
      * Requires `@cyclonedx/bom` `^3.8.0` now, was `^3.1.1`. (via [#287])
    * Misc
      * Added reproducible test environments for unit an integration tests. (via [#51])

---
+++ tests/integration/index.test.js
@@ -0,0 +1,146 @@
+/* eslint-env jest */
+
+/*
+ * This file is part of CycloneDX Webpack plugin.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) OWASP Foundation. All Rights Reserved.
+ */
+
+const fs = require('fs')
+const path = require('path')
+const { spawnSync } = require('child_process')
+const program = require('../../package.json')
+
+describe('integration', () => {
+  describe.each(
+    [
+      {
+        dir: 'webpack4-vue2',
+        purpose: 'webpack4 with vue2',
+        results: [ // paths relative to `dir`
+          {
+            format: 'xml',
+            file: 'dist/.bom/bom.xml'
+          },
+          {
+            format: 'json',
+            file: 'dist/.bom/bom.json'
+          },
+          {
+            format: 'json',
+            file: 'dist/.well-known/sbom'
+          }
+        ]
+      }
+    ]
+  )('$purpose', ({ dir, results }) => {
+
+    const built = spawnSync(
+      'npm', ['run', 'build'], {
+        cwd: path.resolve(__dirname, dir),
+        stdio: ['ignore', 'pipe', 'pipe'],
+        encoding: 'utf8',
+        shell: true,
+        env: {
+          PATH: process.env.PATH,
+          BOM_REPRODUCIBLE: '1'
+        }
+      }
+    )
+    try {
+      expect(built.status).toBe(0)
+    } catch (err) {
+      console.log(built, '\n')
+      throw err
+    }
+
+    it.each(results)('generated $format file: $file', ({ format, file }) => {
+      const resultFile = path.resolve(__dirname, dir, file)
+      const resultBuffer = fs.readFileSync(resultFile)
+      expect(resultBuffer).toBeInstanceOf(Buffer)
+      expect(resultBuffer.length).toBeGreaterThan(0)
+      const resultReproducible = makeReproducible(format, resultBuffer.toString())
+      expect(resultReproducible).toMatchSnapshot()
+    })
+
+  })
+})
+
+/**
+ * @param {string} format
+ * @param {any} data
+ * @returns {string}
+ */
+function makeReproducible (format, data) {
+  switch (format.toLowerCase()) {
+    case 'xml':
+      return makeXmlReproducible(data)
+    case 'json':
+      return makeJsonReproducible(data)
+    default:
+      throw new RangeError(`unexpected format: ${format}`)
+  }
+}
+
+/**
+ * @param {string} json
+ * @returns {string}
+ */
+function makeJsonReproducible (json) {
+  return json
+    .replace(
+      `
+    "tools": [
+      {
+        "vendor": "CycloneDX",
+        "name": "webpack-plugin",
+        "version": "${program.version}"
+      }
+    ]
+`,
+      `
+    "tools": [
+      {
+        "vendor": "CycloneDX",
+        "name": "webpack-plugin",
+        "version": "TESTING"
+      }
+    ]
+`
+    )
+}
+
+/**
+ * @param {string} xml
+ * @returns {string}
+ */
+function makeXmlReproducible (xml) {
+  return xml
+    .replace(`
+      <tool>
+        <vendor>CycloneDX</vendor>
+        <name>webpack-plugin</name>
+        <version>${program.version}</version>
+      </tool>
+`,
+      `
+      <tool>
+        <vendor>CycloneDX</vendor>
+        <name>webpack-plugin</name>
+        <version>TESTING</version>
+      </tool>
+`)
+}
---
+++ tests/integration/webpack4-vue2/README.md
@@ -0,0 +1,7 @@
+## Test: Vue.js setup
+
+This setup is intended to create reproducible results (SBoM).  
+It might install outdated, unmaintained or vulnerable components, for showcasing purposes.
+
+The setup was pulled from DependencyTrack-Frontend v4.4.0.
+See https://github.com/DependencyTrack/frontend/blob/v4.4.0
