e207a84f25d8650ca97760bf0daf717f1a979050
Jan Kowalleck <jan.kowalleck@gmail.com>
2023-02-16 20:13:02 +0100

    chore: get rid of dependency `read-pkg-up`
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

---
+++ src/_helpers.ts
@@ -0,0 +1,49 @@
+/*!
+This file is part of CycloneDX Webpack plugin.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+SPDX-License-Identifier: Apache-2.0
+Copyright (c) OWASP Foundation. All Rights Reserved.
+*/
+
+import { existsSync } from 'fs'
+import { dirname, isAbsolute, join } from 'path'
+
+export interface PackageDescription {
+  path: string
+  packageJson: any
+}
+
+export function getPackageDescription (path: string): PackageDescription | undefined {
+  while (isAbsolute(path)) {
+    const packageJson = join(path, 'package.json')
+    if (existsSync(packageJson)) {
+      try {
+        return {
+          path: packageJson,
+          packageJson: require(packageJson)
+        }
+      } catch {
+        return undefined
+      }
+    }
+
+    const nextPath = dirname(path)
+    if (nextPath === path) {
+      return undefined
+    }
+    path = nextPath
+  }
+  return undefined
+}
