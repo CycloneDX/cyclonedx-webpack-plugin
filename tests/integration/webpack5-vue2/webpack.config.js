0b39983a36cec9d02cc2ecac3e3405b7eea3fcab
Jan Kowalleck <jan.kowalleck@gmail.com>
2023-12-01 18:37:15 +0100

    test: detailed logs in integration tests
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tests/integration/webpack5-vue2/webpack.config.js
+++ tests/integration/webpack5-vue2/webpack.config.js
@@ -6,6 +6,8 @@ const { CycloneDxWebpackPlugin } = require('@cyclonedx/webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
+  // Webpack noise constrained to ALL details - for debugging purposes
+  stats: 'detailed',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
