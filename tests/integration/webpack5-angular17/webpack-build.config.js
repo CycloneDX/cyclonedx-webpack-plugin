3aa488c477fe8bbe4ea2adce83b3e03eb39b752f
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-03-20 14:43:34 +0100

    tests: integrate Angular17 (#1263)
    ---------
    
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

---
+++ tests/integration/webpack5-angular17/webpack-build.config.js
@@ -0,0 +1,14 @@
+const { CycloneDxWebpackPlugin } = require('@cyclonedx/webpack-plugin')
+
+module.exports = {
+  // Webpack noise constrained to ALL details - for debugging purposes
+  // stats: 'detailed',
+  plugins: [
+    new CycloneDxWebpackPlugin({
+      specVersion: '1.5',
+      outputLocation: '.bom',
+      reproducibleResults: true,
+      validateResults: true
+    })
+  ]
+}
