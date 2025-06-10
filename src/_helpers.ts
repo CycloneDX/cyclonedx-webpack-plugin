729aeac6a5b7da55c7e84bfcab4d6e3bc136724c
Jan Kowalleck <jan.kowalleck@gmail.com>
2025-06-10 16:57:02 +0200

    feat: use libraries license gathering
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- src/_helpers.ts
+++ src/_helpers.ts
@@ -103,49 +103,6 @@ export function loadJsonFile(path: string): any {
   // see https://github.com/tc39/proposal-import-attributes
 }
 
-// region MIME
-
-export type MimeType = string
-
-const MIME_TEXT_PLAIN: MimeType = 'text/plain'
-
-const MAP_TEXT_EXTENSION_MIME: Readonly<Record<string, MimeType>> = {
-  '': MIME_TEXT_PLAIN,
-  // https://www.iana.org/assignments/media-types/media-types.xhtml
-  '.csv': 'text/csv',
-  '.htm': 'text/html',
-  '.html': 'text/html',
-  '.md': 'text/markdown',
-  '.txt': MIME_TEXT_PLAIN,
-  '.rst': 'text/prs.fallenstein.rst',
-  '.xml': 'text/xml', // not `application/xml` -- our scope is text!
-  // add more mime types above this line. pull-requests welcome!
-  // license-specific files
-  '.license': MIME_TEXT_PLAIN,
-  '.licence': MIME_TEXT_PLAIN
-} as const
-
-export function getMimeForTextFile(filename: string): MimeType | undefined {
-  return MAP_TEXT_EXTENSION_MIME[extname(filename).toLowerCase()]
-}
-
-const LICENSE_FILENAME_BASE = new Set(['licence', 'license'])
-const LICENSE_FILENAME_EXT = new Set([
-  '.apache',
-  '.bsd',
-  '.gpl',
-  '.mit'
-])
-
-export function getMimeForLicenseFile(filename: string): MimeType | undefined {
-  const {name, ext} = parse(filename.toLowerCase())
-  return LICENSE_FILENAME_BASE.has(name) && LICENSE_FILENAME_EXT.has(ext)
-    ? MIME_TEXT_PLAIN
-    : MAP_TEXT_EXTENSION_MIME[ext]
-}
-
-// endregion MIME
-
 // region polyfills
 
 /** Polyfill for Iterator.some() */
